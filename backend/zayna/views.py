7446d13df821b01df47a390cd5a289be50ceecff
NikPeg <peganov.nik@gmail.com>
2024-08-09 12:31:19 +0300

    login_view

--- backend/zayna/views.py
+++ backend/zayna/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        id = request.POST.get("id")
+        username = request.POST.get("username")
+        user_qs = User.objects.filter(id=id)
+        if user_qs.exists():
+            return HttpResponse(status=204)
+        else:
+            User.objects.create_user(username=username, id=id)
+            return HttpResponse(status=201)
+    return HttpResponse(status=404)
