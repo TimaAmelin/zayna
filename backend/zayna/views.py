b871177c15cc600f735566fcf9b68d4bf08ff833
NikPeg <peganov.nik@gmail.com>
2024-08-11 18:06:06 +0300

    logging

--- backend/zayna/views.py
+++ backend/zayna/views.py
@@ -2,17 +2,23 @@ import json
 
 from .models import User
 from django.http import HttpResponse
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def login_view(request):
     if request.method == "PUT":
+        logger.info(f"[Zayna] Login {request} {request.body}")
         body = json.loads(request.body)
         id = body.get("id")
         username = body.get("username")
         user_qs = User.objects.filter(id=id)
         if user_qs.exists():
+            logger.info(f"[Zayna] User {id} already exists")
             return HttpResponse(status=204)
         else:
+            logger.info(f"[Zayna] Create user {id} with username {username}")
             User.objects.create(username=username, id=id)
             return HttpResponse(status=201)
     return HttpResponse(status=404)
