a6262837e99b7900f4c8bffde5eff77b4d3bb98f
NikPeg <peganov.nik@gmail.com>
2024-08-12 22:35:40 +0300

    logging

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,11 +108,18 @@ AUTH_PASSWORD_VALIDATORS = [
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+    },
     'handlers': {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
             'filename': 'logfile.log',
+            'formatter': 'verbose',
         },
     },
     'root': {
--- backend/zayna/views.py
+++ backend/zayna/views.py
@@ -5,26 +5,19 @@ from django.http import HttpResponse
 
 from .models import User
 
-# logging.basicConfig(
-#     format='%(asctime)s %(levelname)-8s %(message)s',
-#     level=logging.INFO,
-#     datefmt='%Y-%m-%d %H:%M:%S'
-# )
-logger = logging.getLogger(__name__)
-
 
 def login_view(request):
     if request.method == "PUT":
-        logger.info(f"[Zayna] Login {request} {request.body}")
+        logging.info(f"[Zayna] Login {request} {request.body}")
         body = json.loads(request.body)
         id = body.get("id")
         username = body.get("username")
         user_qs = User.objects.filter(id=id)
         if user_qs.exists():
-            logger.info(f"[Zayna] User {id} already exists")
+            logging.info(f"[Zayna] User {id} already exists")
             return HttpResponse(status=204)
         else:
-            logger.info(f"[Zayna] Create user {id} with username {username}")
+            logging.info(f"[Zayna] Create user {id} with username {username}")
             User.objects.create(username=username, id=id)
             return HttpResponse(status=201)
     return HttpResponse(status=404)
