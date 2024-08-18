d623043285e1e172eb900e788747c272263c6840
Timofey <tima.amelin@gmail.com>
2024-08-18 19:19:57 +0300

    ZAYNA-8: tapper screen

--- services/client/src/components/NavbarProvider/NavbarProvider.tsx
+++ services/client/src/components/NavbarProvider/NavbarProvider.tsx
@@ -1,27 +1,36 @@
-'use client'
+'use client';
 
 import React from 'react';
-import { NavbarProviderContainer, NavbarProviderChildrenContainer } from './NavbarProvider.css';
-import { BottomNavigation, BottomNavigationAction } from '@mui/material';
-import { Restore, Favorite, LocationOn } from '@mui/icons-material';
+import { NavbarProviderContainer, NavbarProviderChildrenContainer, NavbarContainer, Navbar } from './NavbarProvider.css';
+import { BottomNavigationAction } from '@mui/material';
 
-import { useRouter, usePathname } from 'next/navigation'
+import { useRouter, usePathname } from 'next/navigation';
+
+import ExchangeIcon from '../../assets/icons/exchange_icon.svg';
+import FriendsIcon from '../../assets/icons/friends_icon.svg';
+import ProjectsIcon from '../../assets/icons/projects_icon.svg';
+import TasksIcon from '../../assets/icons/tasks_icon.svg';
 
 const links = [
     {
-        path: '/',
-        label: 'Home',
-        icon: <Restore />
+        path: '/tapper',
+        label: 'Exchange',
+        icon: <ExchangeIcon />,
     },
     {
-        path: '/path1',
-        label: 'Path1',
-        icon: <Favorite />
+        path: '/project',
+        label: 'Project',
+        icon: <FriendsIcon />,
     },
     {
-        path: '/path2',
-        label: 'Path2',
-        icon: <LocationOn />
+        path: '/friends',
+        label: 'Friends',
+        icon: <ProjectsIcon />,
+    },
+    {
+        path: '/tasks',
+        label: 'Tasks',
+        icon: <TasksIcon />,
     },
 ];
 
@@ -38,18 +47,29 @@ export const NavbarProvider = ({
             <NavbarProviderChildrenContainer>
                 {children}
             </NavbarProviderChildrenContainer>
-            {/* <BottomNavigation
-                showLabels
-                    onChange={(_, newValue) => {
-                        router.push(links[newValue].path);
-                    }}
-                >
-                {links.map((link, index) => (
-                    <BottomNavigationAction key={index} label={link.label} icon={React.cloneElement(link.icon, {
-                        color: pathname === link.path ? 'error' : 'disabled'
-                    })} />
-                ))}
-            </BottomNavigation> */}
+            {pathname !== '/' && (
+                <NavbarContainer>
+                    <Navbar
+                        showLabels
+                            onChange={(_, newValue) => {
+                                router.push(links[newValue].path);
+                            }}
+                        >
+                        {links.map((link, index) => (
+                            <BottomNavigationAction
+                                sx={{
+                                    backgroundColor: '#2E3342',
+                                    color: pathname === link.path ? '#7ADA46' : '#999',
+                                    borderRadius: index === 0 ? '5px 0 0 5px' : index === links.length - 1 ? '0 5px 5px 0' : 0,
+                                    height: 65,
+                                }}
+                                key={index} label={link.label}
+                                icon={React.cloneElement(link.icon, {
+                                    style: {filter: pathname === link.path ? 'sepia(0.5) hue-rotate(50deg) saturate(4) brightness(1) contrast(2)' : 'none'}
+                                })} />
+                        ))}
+                    </Navbar>
+                </NavbarContainer>)}
         </NavbarProviderContainer>
     )
 }
