cb9e9d831dacb577ba36ea670f57deadca9974ce
NikPeg <peganov.nik@gmail.com>
2024-08-20 16:02:37 +0300

    tokens migration

--- backend/zayna/models.py
+++ backend/zayna/models.py
@@ -5,3 +5,9 @@ class User(models.Model):
     id = models.IntegerField(primary_key=True)
     username = models.CharField(max_length=32, null=True, blank=True)
     referrer = models.ForeignKey("User", null=True, on_delete=models.SET_NULL)
+    tokens = models.TextField(default="0")
+
+
+class TokensBatch(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    tokens = models.IntegerField(default=0)
