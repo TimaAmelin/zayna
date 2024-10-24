2ea138e07dd94ad4e29c525db83f5c62157cd2bf
Timofey <tima.amelin@gmail.com>
2024-10-25 02:43:10 +0300

    zayna images

--- services/client/src/components/Projects/Projects.tsx
+++ services/client/src/components/Projects/Projects.tsx
@@ -14,11 +14,6 @@ import {
   ProjectsMainContainerMoney,
   ProjectsMainContainerTypeButton,
   ProjectsMainContainerTypeRow,
-} from './Projects.css';
-import { getProjects } from '@/api/handlers/getProjects';
-import { getTokens } from '@/api/handlers/getTokens';
-
-import {
   ProjectsStatisticsContainer,
   ProjectsStatistics,
   ProjectsStatisticsCoinContainer,
@@ -33,6 +28,9 @@ import {
   ProjectsStatisticsName,
 } from './Projects.css';
 
+import { getProjects } from '@/api/handlers/getProjects';
+import { getTokens } from '@/api/handlers/getTokens';
+
 import CoinMin from '../../assets/icons/coin_min.svg';
 import CoinIcon from '../../assets/icons/coin_big.png';
 import IIcon from '../../assets/icons/i.svg';
@@ -43,6 +41,48 @@ import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import { Box } from '@mui/material';
 import { ProjectModal } from '../ProjectModal/ProjectModal';
+
+import Ekstrennyenovosti1 from '../../assets/logos/ekstrennyenovosti1.svg';
+import Kitenok from '../../assets/logos/kitenok.svg';
+import Klimaticheskiyfond from '../../assets/logos/klimaticheskiyfond.svg';
+import Lesnik from '../../assets/logos/lesnik.svg';
+import Marsianskiykolonist from '../../assets/logos/marsianskiykolonist.svg';
+import Meditsinskayamissiya from '../../assets/logos/meditsinskayamissiya.svg';
+import Mirnoenastroenie from '../../assets/logos/mirnoenastroenie.svg';
+import Mevidimyyzashchitnik from '../../assets/logos/nevidimyyzashchitnik.svg';
+import Pandapatrul from '../../assets/logos/pandapatrul.svg';
+import Planetaschastya from '../../assets/logos/planetaschastya.svg';
+import Povelitelbur from '../../assets/logos/povelitelbur.svg';
+import Poymaydzen from '../../assets/logos/poymaydzen.svg';
+import Skorayapomoshch from '../../assets/logos/skorayapomoshch.svg';
+import Solnechnayabatareya from '../../assets/logos/solnechnayabatareya.svg';
+import Sportsportsport from '../../assets/logos/sportsportsport.svg';
+import Terapiyadobra from '../../assets/logos/terapiyadobra.svg';
+import Zaryadiakkumulyatory from '../../assets/logos/zaryadiakkumulyatory.svg';
+import Zelenyydrug from '../../assets/logos/zelenyydrug.svg';
+import Zelenyydzheday from '../../assets/logos/zelenyydzheday.svg';
+
+// const logosObj = {
+//   ekstrennyenovosti1,
+//   kitenok,
+//   klimaticheskiyfond,
+//   lesnik,
+//   marsianskiykolonist,
+//   meditsinskayamissiya,
+//   mirnoenastroenie,
+//   nevidimyyzashchitnik,
+//   pandapatrul,
+//   planetaschastya,
+//   povelitelbur,
+//   poymaydzen,
+//   skorayapomoshch,
+//   solnechnayabatareya,
+//   sportsportsport,
+//   terapiyadobra,
+//   zaryadiakkumulyatory,
+//   zelenyydrug,
+//   zelenyydzheday,
+// }
 
 export const Projects = () => {
   const [projects, setProjects] = useState<
@@ -61,7 +101,7 @@ export const Projects = () => {
   const [money, setMoney] = useState(0);
   const [moneyPerHour, setMoneyPerHour] = useState(0);
 
-  const [type, setType] = useState('Forests');
+  const [type, setType] = useState('Рынок');
 
   const [open, setOpen] = useState(false);
   const [project, setProject] = useState(projects[0]);
@@ -183,13 +223,7 @@ export const Projects = () => {
                   setOpen(true);
                 }}
               >
-                <img
-                  src={project.logo.replace('web', '45.159.188.244').replace(/\_[a-zA-Z0-9]+\./g, '.')}
-                  alt=""
-                  height={38}
-                  width={38}
-                />
-                {project.logo.replace('web', '45.159.188.244').replace(/\_[a-zA-Z0-9]+\./g, '.')}
+                <Kitenok style={{height: 38}} />
                 <ProjectsMainContainerCardTitle>
                   {project.name}
                 </ProjectsMainContainerCardTitle>
