957eec2a90647882b78236976e8a10c33b813f37
NikPeg <peganov.nik@gmail.com>
2024-10-31 12:19:34 +0300

    import

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "backend.backend.settings.BlockedIpMiddleware",
+    "BlockedIpMiddleware",
 ]
 
 ROOT_URLCONF = "backend.urls"
